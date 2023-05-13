6dbe67a0cf275b651a1bf5f5a672192ab042e809
Jesserc <raymondjesse713@gmail.com>
2023-05-13 17:10:27 +0100

    chore: forge init

--- 5GuessRandomNumber/script/Counter.s.sol
+++ 5GuessRandomNumber/script/Counter.s.sol
@@ -1,37 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
-import "../src/GuessNewNumber.sol";
+import "forge-std/Script.sol";
 
-contract GuessNewNumberTest is Test {
-    GuessNewNumber public guessNewNumber;
-    ExploitContract public exploitContract;
-
+contract CounterScript is Script {
     function setUp() public {}
 
-    function testAnswer(uint256 blocnumber, uint256 bloctimestamp) public {
-        vm.assume(blocnumber !=0);
-        vm.assume(bloctimestamp !=0);
-        //time frame
-        vm.roll(blocnumber);
-        vm.warp(bloctimestamp);
-
-        //Set up
-        guessNewNumber = (new GuessNewNumber){value: 1 ether}();
-        exploitContract = new ExploitContract();
-
-        // Your code below
-        uint8 guess = exploitContract.Exploit();
-
-
-        guessNewNumber.guess{value: 1 ether}(guess);
-
-        // Do not modify the lines of code below
-
-        assertTrue(guessNewNumber.isComplete(), "Wrong Number");
+    function run() public {
+        vm.broadcast();
     }
-
-    receive() external payable {}
 }
