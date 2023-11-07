90fb69e908051db87aeaeb7949a99aef880e0a1a
smol-ninja <shubhamy2015@gmail.com>
2023-11-07 15:37:41 +0000

    define a separate owner in RetirementFund.t.sol
    `msg.sender` is both owner as well as beneficiary. For this CTF to be challenging, owner should be different than beneficiary.
    
    I propose `RetirementFundTest` as beneficiary and address generated by `makeAddr("owner")` as the owner of `RetirementFund`.

--- RetirementFund/test/RetirementFund.t.sol
+++ RetirementFund/test/RetirementFund.t.sol
@@ -8,8 +8,14 @@ contract RetirementFundTest is Test {
     RetirementFund public retirementFund;
     ExploitContract public exploitContract;
 
+    // owner that will deploy the contract. Do not use it as a prank.
+    address owner = makeAddr("owner");
+
     function setUp() public {
         // Deploy contracts
+        vm.deal(owner, 1 ether);
+        // use owner to deploy the contract
+        vm.prank(owner);
         retirementFund = (new RetirementFund){value: 1 ether}(address(this));
         exploitContract = new ExploitContract(retirementFund);
     }
